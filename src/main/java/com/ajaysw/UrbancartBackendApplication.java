f1a5bef1b01783df091a82d33e05ef6d6f006a4e
Ajay Wankhade <ajaysw45@gmail.com>
2024-10-03 12:28:51 +0530

    project initialized

---
+++ src/main/java/com/ajaysw/UrbancartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ajaysw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbancartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbancartBackendApplication.class, args);
+    }
+
+}
